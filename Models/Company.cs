eb9b5ea6191e25d56e3ba8c61e5343c68308a590
Eduardo <ecoliveira0609@gmail.com>
2025-06-17 10:48:27 +0100

    Models added

---
+++ Models/Company.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+    public class Company
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string NIF { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+        public string Status { get; set; } = "Draft";
+        public string Stakeholder { get; set; }
+        public string Contact { get; set; }
+    }
+}
---
+++ Models/Proposal.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+    public class Proposal
+    {
+        public Guid ProposalID { get; set; } = Guid.NewGuid();
+        public Guid LeadID { get; set; }
+        public List<Guid> ProductIDs { get; set; } = new();
+        public decimal ProductionCost { get; set; }
+        public int MonthlyProducedProducts { get; set; }
+        public decimal ExpectedMonthlyProfit { get; set; }
+        public string Status { get; set; } = "Draft";
+    }
+}
