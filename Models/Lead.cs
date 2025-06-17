8051ff47fcf399605afdc59787df89e23615da03
Eduardo <ecoliveira0609@gmail.com>
2025-06-17 11:04:19 +0100

    Missing Models

--- Models/Lead.cs
+++ Models/Lead.cs
@@ -2,5 +2,10 @@
 {
     public class Lead
     {
+      public Guid LeadID { get; set; } = Guid.NewGuid(); 
+      public Guid CompanyId { get; set; } 
+      public string Country { get; set; } 
+      public string BusinessType { get; set; } 
+      public string Status { get; set; } = "Draft"; 
     }
 }
